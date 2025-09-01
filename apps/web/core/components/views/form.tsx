2b69fcb17647b8db5cc402f93385be508ca0a4ef
Nicholas Paulik <nicholas.paulik@schibsted.com>
2025-09-01 16:22:01 +0200

    FEATURE: show_empty_groups default when creating view

--- apps/web/core/components/views/form.tsx
+++ apps/web/core/components/views/form.tsx
@@ -48,7 +48,7 @@ const defaultValues: Partial<IProjectView> = {
   description: "",
   access: EViewAccess.PUBLIC,
   display_properties: getComputedDisplayProperties(),
-  display_filters: { ...getComputedDisplayFilters(), group_by: "state" },
+  display_filters: { ...getComputedDisplayFilters(), group_by: "state", show_empty_groups: true },
 };
 
 export const ProjectViewForm: React.FC<Props> = observer((props) => {
